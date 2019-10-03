89e7949b27b85b29ec2d1de412066fe422c8e822
at1208 <amantiwari12081999@gmail.com>
2019-10-03 07:23:22 +0530

    style improved

--- src/components/calculator/calci.js
+++ src/components/calculator/calci.js
@@ -4,32 +4,28 @@ import Fade from 'react-reveal/Fade';
 
 class Calsi extends Component {
   state = {
-    b1: null,
-    b2: null,
-    b3: null,
-    b4: null,
-    b5: null,
-    b6: null,
-    b7: null,
-    b8: null,
-    b9: null,
-    b0: null,
-
-    operator1: null,
-    operator2: null,
-    operator3: null,
-    operator4: null,
-
 
     DARK: false,
     LIGHT: true,
 
-
-
-
   }
 
+plusOperation = () => {
+ 
+  return
+}
 
+minusOperation = () => {
+  return
+}
+
+multiplyOperation = () => {
+  return
+}
+
+divideOperation = () => {
+  return
+}
 
   render(){
 if(this.state.LIGHT){
@@ -56,7 +52,7 @@ if(this.state.LIGHT){
                      </div>
                   </Fade>
 
-                   <div className='a3 container-fluid'>
+                   <div className='a3 container'>
                               <Fade bottom>
                                  <div className='a4 container shadow text-center'>
                                  </div>
@@ -64,27 +60,27 @@ if(this.state.LIGHT){
 
                               <Fade bottom>
                                  <div className='row'>
-                                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b1: 1 })}>1</button>
-                                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b2: 2 })}>2</button>
-                                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b3: 3 })}>3</button>
+                                  <button className='a2 text-center btn ' >1</button>
+                                  <button className='a2 text-center btn '  >2</button>
+                                  <button className='a2 text-center btn '  >3</button>
                                   <button className='a2 text-center btn ' >+</button>
                                  </div>
                              </Fade>
 
                            <Fade bottom>
                                <div className='row'>
-                                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b4: 4 })}>4</button>
-                                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b5: 5})}>5</button>
-                                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b6: 6})}>6</button>
+                                  <button className='a2 text-center btn '  >4</button>
+                                  <button className='a2 text-center btn '  >5</button>
+                                  <button className='a2 text-center btn '  >6</button>
                                   <button className='a2 text-center btn '  >-</button>
                                </div>
                            </Fade>
 
                            <Fade bottom>
                               <div className='row'>
-                                <button className='a2 text-center btn ' onClick = {() => this.setState({ b7: 7 })}>7</button>
-                                <button className='a2 text-center btn ' onClick = {() => this.setState({ b8: 8 })}>8</button>
-                                <button className='a2 text-center btn ' onClick = {() => this.setState({ b9: 9 })}>9</button>
+                                <button className='a2 text-center btn '  >7</button>
+                                <button className='a2 text-center btn '  >8</button>
+                                <button className='a2 text-center btn '  >9</button>
                                 <button className='a2 text-center btn '>*</button>
                               </div>
                            </Fade>
@@ -92,7 +88,7 @@ if(this.state.LIGHT){
                           <Fade bottom>
                             <div className='row'>
                                <button className='a2 text-center btn '>Clear</button>
-                               <button className='a2 text-center btn 'onClick = {() => this.setState({ b0: 0 })}>0</button>
+                               <button className='a2 text-center btn ' >0</button>
                                <button className='a2 text-center btn '>=</button>
                                <button className='a2 text-center btn '>/</button>
                             </div>
@@ -134,27 +130,27 @@ if(this.state.LIGHT){
 
                     <Fade bottom>
                        <div className='row'>
-                        <button className='a2 text-center btn ' onClick = {() => this.setState({ b1: 1 })}>1</button>
-                        <button className='a2 text-center btn ' onClick = {() => this.setState({ b2: 2 })}>2</button>
-                        <button className='a2 text-center btn ' onClick = {() => this.setState({ b3: 3 })}>3</button>
-                        <button className='a2 text-center btn ' >+</button>
+                        <button className='a2 text-center btn '  >1</button>
+                        <button className='a2 text-center btn '  >2</button>
+                        <button className='a2 text-center btn '  >3</button>
+                        <button className='a2 text-center btn '  >+</button>
                        </div>
                    </Fade>
 
              <Fade bottom>
                  <div className='row'>
-                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b4: 4 })}>4</button>
-                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b5: 5})}>5</button>
-                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b6: 6})}>6</button>
+                  <button className='a2 text-center btn '  >4</button>
+                  <button className='a2 text-center btn '  >5</button>
+                  <button className='a2 text-center btn '  >6</button>
                   <button className='a2 text-center btn '  >-</button>
                  </div>
              </Fade>
 
               <Fade bottom>
                  <div className='row'>
-                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b7: 7 })}>7</button>
-                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b8: 8 })}>8</button>
-                  <button className='a2 text-center btn ' onClick = {() => this.setState({ b9: 9 })}>9</button>
+                  <button className='a2 text-center btn '  >7</button>
+                  <button className='a2 text-center btn '  >8</button>
+                  <button className='a2 text-center btn '  >9</button>
                   <button className='a2 text-center btn '>*</button>
                   </div>
                </Fade>
@@ -162,7 +158,7 @@ if(this.state.LIGHT){
                  <Fade bottom>
                   <div className='row'>
                    <button className='a2 text-center btn '>Clear</button>
-                   <button className='a2 text-center btn 'onClick = {() => this.setState({ b0: 0 })}>0</button>
+                   <button className='a2 text-center btn ' >0</button>
                    <button className='a2 text-center btn '>=</button>
                    <button className='a2 text-center btn '>/</button>
                   </div>
